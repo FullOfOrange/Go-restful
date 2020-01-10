cc03d6b0a03ab28edd610ac421e7ffc1ba1fc8ef
FullOfOrange <foorgdev@gmail.com>
2020-01-11 01:01:21 +0900

    docs: add goroutine

---
+++ examples/goroutine.go
@@ -0,0 +1,28 @@
+package main
+ 
+import (
+    "fmt"
+    "sync"
+)
+ 
+func main() {
+    // WaitGroup 생성. 2개의 Go루틴을 기다림.
+    var wait sync.WaitGroup
+    wait.Add(6)
+ 
+    // 익명함수를 사용한 goroutine
+    go func() {
+        defer wait.Done() //끝나면 .Done() 호출
+        fmt.Println("Hello")
+    }()
+ 
+	for i := 0; i < 5; i++{
+    	// 익명함수에 파라미터 전달
+		go func(msg string) {
+			defer wait.Done() //끝나면 .Done() 호출
+			fmt.Println(msg)
+		}("Hi")
+	}
+ 
+    wait.Wait() //Go루틴 모두 끝날 때까지 대기
+}
